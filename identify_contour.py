50bf6105380d9ef1d1c844ebb96e5b5a08017482
prastabdkl <prastabdkl@gmail.com>
2022-01-22 01:40:56 -0600

    implement retr_tree for hierarchy

--- identify_contour.py
+++ identify_contour.py
@@ -18,6 +18,7 @@ path,dirs,files = next(os.walk(jpg_folder_path))
 file_count = len(files)
 
 max_image_area = 0
+file_count = 2
 for i in range(1, file_count):
 # for i in range(1, 10):
     img_path = r'C:\Users\prast\Downloads\CS 7000\Final report\COVID-CT-MD\output\covid\Covid_'+f'{str(i)}.jpg' 
@@ -30,11 +31,28 @@ for i in range(1, file_count):
     # thresh = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
     #cv2.bitwise_not(thresh, thresh)
 
-    cnts = cv2.findContours(thresh, cv2.RETR_CCOMP,
+    cnts = cv2.findContours(thresh, cv2.RETR_TREE,
                     cv2.CHAIN_APPROX_SIMPLE)
-    cnts = cnts[0]
+    
+    contours,hierarchy = cv2.findContours(thresh, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+    hierarchy = hierarchy[0] # get the actual inner list of hierarchy descriptions
+    
+    # cv2.drawContours(org_img, hierarchy, -1, (0,255,255), 2)
+    print(type(contours))
+    print(type(hierarchy))    
+    break;
+    # for cnt in cnts:
+    #     for i in cnt:
+    #         for j in i:                
+    #             print(j)
+    
+    # cnts = cnts[0]
+    # for cnt in cnts:
+    #     print("Countours", cnt[3])
+    
     max_contour = max(cnts, key=cv2.contourArea)
-    # print("Countours", c)
+    # print("Countours", cnts)
 
     # left = tuple(c[c[:, :, 0].argmin()][0])
     # right = tuple(c[c[:, :, 0].argmax()][0])
@@ -58,18 +76,18 @@ for i in range(1, file_count):
     # cv2.putText(img,'Distance: '+str(distance),(10,30), font, 1, (0,0,0),2, cv2.LINE_AA)
 
     total_area = 0
-    cnt_with_max_area = cnts[0]
+    # cnt_with_max_area = cnts[0]
     
     for cnt in cnts:
-        cv2.drawContours(org_img, [cnt], -1, (0,255,255), 2)
+        # cv2.drawContours(org_img, [cnt], -1, (0,255,255), 2)
         total_area += cv2.contourArea(cnt)
         
-        if cv2.contourArea(cnt) > cv2.contourArea(cnt_with_max_area):
-            cnt_with_max_area = cnt
+        # if cv2.contourArea(cnt) > cv2.contourArea(cnt_with_max_area):
+        #     cnt_with_max_area = cnt
     
-    print(cnt_with_max_area)
-    index = cnts.index(cnt_with_max_area)
-    print(index)
+    # print(cnt_with_max_area)
+    # index = cnts.index(cnt_with_max_area)
+    # print(index)
     # mask = np.zeros_like(img) # Create mask where white is what we want, black otherwise
     # cv2.drawContours(mask, cnts, idx, 255, -1) # Draw filled contour in mask
     # out = np.zeros_like(img) # Extract out the object and place into output image
@@ -85,7 +103,7 @@ for i in range(1, file_count):
     # cv2.imshow('Output', out)
     # cv2.waitKey(0)
     # cv2.destroyAllWindows()
-    break;    
+    # break;    
     # cv2.drawContours(org_img, [cnt], -1, (0,255,255), 2)
         
     
